337a27127c001d9b41c50787f1503db4247a62d8
Sudharsan B <25sudharsan27@gmail.com>
2023-12-13 14:23:15 +0530

    path module 1

--- 10-pathmodule.js
+++ 10-pathmodule.js
@@ -6,3 +6,9 @@ console.log(path.sep);
 const filepath=path.join('/content','/subfolder','sudharsan.txt');
 
 console.log(filepath);
+
+const base =path.basename(filepath);
+console.log(base)
+
+const absolute = path.resolve(__dirname,'content','subfolder','sudharsan.txt');
+console.log(absolute);
